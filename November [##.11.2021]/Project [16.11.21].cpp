74d0aee392e4e9d67a99ac5fc17175f36bee63a0
printWhoIsHere <81147316+printWhoIsHere@users.noreply.github.com>
2021-11-24 01:32:22 +0300

    Викторина

--- November [##.11.2021]/Project [16.11.21].cpp
+++ November [##.11.2021]/Project [16.11.21].cpp
@@ -2,15 +2,15 @@
 #include <iostream>
 #include <windows.h>
 #include <sstream>
-//#include <cmath>
 #include <string>
 #include <ctime>
-//#include <ratio>
-//#include <chrono>
-//Таймер только на вопрос (~1 мин на вопрос)
+#include <thread>
+
 using namespace std;
 
-size_t Minutes = 1; //сколько минут отведем на игру
+size_t Minutes = 5; // Сколько минут отведено на игру
+size_t Seconds = 0; // Отчёт всегда с ровным минут
+
 int numberOfQuestions = 15;
 int countOfAttempts = 3;
 
@@ -23,6 +23,37 @@ bool playingGame(time_t i);
 void clear() {
     system("cls");
 }
+
+
+// ТАЙМЕР
+//int taimer() {
+//    cout << "\x1b[36m\n";
+//    cout.fill('0');
+//    while (true)
+//    {
+//        cout.width(2);
+//        cout << Minutes << ":";
+//        cout.width(2);
+//        cout << Seconds;
+//        Sleep(970);
+//        if (Seconds > 0)
+//        {
+//            Seconds--;
+//        }
+//        else if (Seconds == 0 && Minutes > 0)
+//        {
+//            Seconds = 59;
+//            Minutes--;
+//        }
+//        else if (Seconds == 0 && Minutes == 0)
+//        {
+//            cout << "Время вышло!";
+//            break;
+//        }
+//    }
+//    cout << "\x1b[0m\n";
+//    return 0;
+//}
 
 // Вопросы
 string Answer[17][2] = {
@@ -40,7 +71,7 @@ string Answer[17][2] = {
         {"Какие две планеты вращаются в обратном направлении от остальных?\n [1] - Уран и Венера\n [2] - Венера и Плутон\n [3] - Меркурий и Юпитер\n [4] - Земля и Нептун","1"},
         {"Назовите самую длинную реку в мире?\n [1] - Нил\n [2] - Амазонка\n [3] - Волга\n [4] - Миссисипи","1"},
         {"Какая страна самая новая в мире, признанная в ООН?\n [1] - Алжир\n [2] - Арабские Эмираты\n [3] - Уагвей\n [4] - Южный Судан","4"},
-        {"Ежесуточно Земля прибавляет в весе 400 тонн. За счет чего?\n [1] - За счёт людей\n [2] - За счёт отходов\n [3] - За счёт космической пыли\n [4] - За счёт еды после 6","3" },
+        {"Ежесуточно Земля прибавляет в весе 400 тонн. За счет чего?\n [1] - За счёт людей\n [2] - За счёт отходов\n [3] - За счёт космической пыли\n [4] - За счёт еды после 18:00","3" },
         {"Где текут реки без воды?\n [1] - В космосе\n [2] - На картах\n [3] - В пустыне\n [4] - В горах", "2"},
         {"Какая валюта Швеции?\n [1] - Шведэн\n [2] - Шведская крона\n [3] - Доллар\n [4] - Шведский франк", "2"}
 };
@@ -66,12 +97,70 @@ void randomizeQuestion() {
     }
 }
 
+void play() {
+    clear();
+    int sum = 0;
+    time_t now = time(0);
+    short fifityFifty = 0;
+    time_t stopPlayTime = now + (60 * Minutes);  //Когда закончить игру
+    for (int i = 0; i < numberOfQuestions and countOfAttempts>0 and playingGame(stopPlayTime); i++) {
+        Sleep(1000);
+        clear();
+        cout << "\n\x1b[36mУ вас осталось: " << countOfAttempts << " жизней\x1b[0m\n";
+    goAnswer:
+        string condition;
+        if (fifityFifty == 1) {
+            help_draw(i);
+            fifityFifty = 3;
+        }
+        else if (fifityFifty == 0)
+            cout << "\x1b[36mЧто бы использовать подсказку напишите: \"50/50\"\x1b[0m\n";
+        cout << "\n\x1b[31m--------------------\x1b[0m\n";
+        cout << Answer[i][0] << "\n\x1b[31m--------------------\x1b[0m\nВведите ответ на вопрос: ";
+        cin >> condition;
+        if (condition == Answer[i][1]) {
+            sum++;
+            cout << "\n\x1b[92mВы ответили правильно!\x1b[0m";
+        }
+        else if (condition == "50/50" and fifityFifty != 3) {
+            fifityFifty = 1;
+            goto goAnswer;
+        }
+        else if (condition != "1" and condition != "2" and condition != "3" and condition != "4") {
+            cout << "\x1b[91mИграй по правилам, ответ от 1 до 4 должен быть!\x1b[0m\n";
+            system("pause");
+            clear();
+            goto goAnswer;
+        }
+        else {
+            cout << "\n\x1b[91mУвы, ответ не верен \nМинус " << static_cast<char>(3) << "\x1b[0m\n";
+            countOfAttempts--;
+        }
+    }
+    cout << "\n\n\n\x1b[91m---------------------------------------------------------------------\n";
+    cout << "\t Игра окончена, вы ответили правильно, на \x1b[92m" << sum << "\x1b[91m вопросов из \x1b[92m" << numberOfQuestions;
+    cout << "\n\x1b[91m---------------------------------------------------------------------\x1b[0m\n";
+}
+
+void easterEgg() {
+    clear();
+    cout << "\n\n***  \"EASTER_EGG.exe\"  ***\t\t\t\n";
+    cout << "\x1b[31m--------------------\x1b[0m\n\n\n";
+    system("pause");
+    while (true)
+    {
+        srand(time(NULL));
+        short smile = rand() % (97 - 91) + 91;
+        cout << "\x1b[" << smile << "mxXxXxXxXxXxXx" << "\x1b[0m";
+    }
+}
+
 void rules() {
     clear();
     cout << "\n\n***  \"Правила\"  ***\t\t\t\n";
     cout << "\x1b[31m--------------------\x1b[0m\n";
-    cout << " Правила просты. \n Приятной игры " << static_cast<char>(2) << "\n";
-    cout << "\x1b[31m--------------------\x1b[0m\n\n";
+    cout << " Правила просты. \n Приятной игры \x1b[92m" << static_cast<char>(2);
+    cout << "\n\x1b[31m--------------------\x1b[0m\n\n";
     system("pause");
     clear();
     menu();
@@ -95,6 +184,10 @@ void settings() {
     case 3:
         cout << "\nВведите, количество минут которые будет длится игра:";
         cin >> Minutes;
+        break;
+    case 9:
+        clear();
+        easterEgg();
         break;
     case 1:
         clear();
@@ -153,52 +246,8 @@ void settings() {
     }
     menu();
 }
-void play() {
-    clear();
-    int sum = 0;
-    time_t now = time(0);
-    short fifityFifty = 0;
-    time_t stopPlayTime = now + (60 * Minutes);  //Когда закончить игру
-    for (int i = 0; i < numberOfQuestions and countOfAttempts>0 and playingGame(stopPlayTime); i++) {
-        Sleep(1000);
-        clear();
-        cout << "\n\x1b[36mУ вас осталось: " << countOfAttempts << " жизней\x1b[0m\n";
-    goAnswer:
-        string condition;
-        if (fifityFifty == 1) {
-            help_draw(i);
-            fifityFifty = 3;
-        }
-        else if (fifityFifty == 0)
-            cout << "\x1b[36mЧто бы использовать подсказку напишите: \"50/50\"\x1b[0m\n";
-        cout << "\n\x1b[31m--------------------\x1b[0m\n";
-        cout << Answer[i][0] << "\n\x1b[31m--------------------\x1b[0m\nВведите ответ на вопрос: ";
-        cin >> condition;
-        if (condition == Answer[i][1]) {
-            sum++;
-            cout << "\n\x1b[92mВы ответили правильно!\x1b[0m";
-        }
-        else if (condition == "50/50" and fifityFifty != 3) {
-            fifityFifty = 1;
-            goto goAnswer;
-        }
-        else if (condition != "1" and condition != "2" and condition != "3" and condition != "4") {
-            cout << "\x1b[91mИграй по правилам, ответ от 1 до 4 должен быть!\x1b[0m\n";
-            system("pause");
-            clear();
-            goto goAnswer;
-        }
-        else {
-            cout << "\n\x1b[91mУвы, ответ не верен \nМинус " << static_cast<char>(3) << "\x1b[0m\n";
-            countOfAttempts--;
-        }
-    }
-    cout << "\n\n\n\x1b[91m---------------------------------------------------------------------\n";
-    cout << "\t Игра окончена, вы ответили правильно, на \x1b[92m" << sum << "\x1b[91m вопросов из \x1b[92m" << numberOfQuestions;
-    cout << "\n\x1b[91m---------------------------------------------------------------------\x1b[0m\n";
-}
 
-//timer
+// Таймер
 bool playingGame(time_t stopTime) {
     char* stopTime2 = ctime(&stopTime);
     cout << "\nИгра закончится в: \n" << stopTime2 << "\n";
@@ -214,6 +263,8 @@ bool playingGame(time_t stopTime) {
 
 void menu() {
     clear();
+    //thread th(taimer);
+    //th.join();
     cout << "\a\n\n***Приветствуем Вас в программе — \"Викторина\"***\t\t\t\n";
     cout << "\x1b[31m————————————————————————————————————————————————————————\x1b[0m";
     cout << "\n[1] Играть";
@@ -241,6 +292,8 @@ void menu() {
         menu();
     }
 }
+
+
 
 int main(int argc, char* argv[]) {
     setlocale(0, "");
